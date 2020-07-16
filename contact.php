532024b734d08ed0c464e51ad4246b3d9f7f12e4
Deepa Baby <deepababy91@gmail.com>
2020-07-16 03:28:08 -0500

    Adding php file to post details of contact us form

---
+++ contact.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+Your first name is:<?php echo $_POST["name"]; ?><br>
+Your last name is :<?php echo $_POST["surname"]; ?><br>
+Your email is     :<?php echo $_POST["email"]; ?><br>
+Your phone is     :<?php echo $_POST["phone"]; ?><br>
+Your message is   :<?php echo $_POST["message"]; ?><br>
+</body>
+</html>
